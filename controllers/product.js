85f773b1053b90075bd9a0a00abd30b86d80a184
Rodrigo <obrido@gmail.com>
2020-07-09 05:05:06 -0400

    mvc implemented, needs refining for further improvement

--- controllers/product.js
+++ controllers/product.js
@@ -1,18 +1,27 @@
-//mock database
-const database = [];
-
+const Product = require("../models/product");
 
 exports.getAddProduct = (req, res, next) => {
-  res.render('add-product', { pageTitle: 'Add Product', path: '/admin/add-product', activeProduct: true });
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    activeProduct: true
+  });
 };
 
 exports.postAddProduct = (req, res, next) => {
-  database.push({ title :req.body.title });
-  res.redirect('/');
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', { prods: database, pageTitle: 'Shop', path: '/', hasProducts: database.length > 0, activeShop: true });
+  Product.fetchAll(products => {
+    res.render("shop", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+      hasProducts: products.length > 0,
+      activeShop: true
+    });
+  });
 };
-
-exports.database = database;
